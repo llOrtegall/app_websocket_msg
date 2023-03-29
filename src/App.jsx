e3083924c9353d617ecaaa5c22bf3a46464d75ff
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-03-28 21:48:19 -0500

    renderizando el numero

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
-import './App.css'
+const TURNS = {
+  X: 'x',
+  O: 'o'
+}
+
+const board = Array(9).fill(null);
 
 function App() {
-  return <h1>Tic Tac Toe</h1>
+  return (
+    <main className="board">
+      <h1>Tic Tac Toe</h1>
+      <section className="game">
+        {
+          board.map((_, index) => {
+            return (
+              <div className="cell" key={index}>
+                <span className="cell_content">
+                  {index}
+                </span>
+              </div>
+            )
+          })
+        }
+      </section>
+    </main>
+  )
 }
 
 export default App
