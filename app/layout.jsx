449073b76558de3b50c9bd3bc6c1ab682a17c555
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-05-07 15:20:43 -0500

    min 1:45 proyect create next

--- app/layout.jsx
+++ app/layout.jsx
@@ -7,17 +7,19 @@ export const metadata = {
   description: " Discover 6 share AI Prompts",
 };
 
-const Rootlayout = ({children}) => {
+const Rootlayout = ({ children }) => {
   return (
     <html lang="en">
       <body>
-        <div className="main">
-          <div className="gradiant" />
-        </div>
-        <main className="app">
-          <Nav/>
-          {children}
-        </main>
+        <Provider>
+          <div className="main">
+            <div className="gradiant" />
+          </div>
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
       </body>
     </html>
   );
