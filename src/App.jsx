580a4eb86a51fc025b0072b3a8ad6aa5168ae756
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-07-26 21:28:02 -0500

    adawdawd

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,67 @@
+import { useState } from "react"
 
+const TURNS = {
+  X: 'x',
+  O: 'o'
+}
 
-function App() {
+const Square = ({ children, isSelected, updateBoard, index }) => {
+  const className = `square ${isSelected ? 'is-selected' : ''}`
+
+  const handleClick = () => {
+    updateBoard(index)
+  }
+
   return (
-    <h1>Tic Tac Toe</h1>
+    <div onClick={handleClick} className={className}>
+      {children}
+    </div>
   )
 }
 
+function App() {
+  const [board, SetBoard] = useState(
+    Array(9).fill(null)
+  )
+
+  const [turn, setTurn] = useState(TURNS.X)
+
+  const updateBoard = (index) => {
+    const newBoard = [...board]
+    newBoard[index] = turn
+    SetBoard(newBoard)
+
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+  }
+  return (
+    <main className="board">
+      <h1 style={{ marginBottom: '20px' }}>Tic Tac Toe</h1>
+      <section className="game">
+        {
+          board.map((_, index) => {
+            return (
+              <Square
+                key={index}
+                index={index}
+                updateBoard={updateBoard}
+              >
+                {board[index]}
+              </Square>
+            )
+          })
+        }
+      </section>
+
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>
+          {TURNS.X}
+        </Square>
+        <Square isSelected={turn === TURNS.O}>
+          {TURNS.O}
+        </Square>
+      </section>
+    </main>
+  )
+}
 export default App
