ef4802f8d0e8084a7b07d6e88e68cf75814f5426
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-07-26 20:50:42 -0500

    desde 0 el tic tac toe

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,8 @@
-import { useState } from "react"
 
-const TURNS = {
-  X: 'x',
-  O: 'o'
-}
-
-
-const Square = ({ children, updateBoard, index }) => {
-  return (
-    <div className="square">
-      {children}
-    </div>
-  )
-}
 
 function App() {
-
-  const [board, setBoard] = useState(
-    Array(9).fill(null)
-  )
-
-  const [turn, setTurn] = useState(TURNS.X)
-
   return (
-    <main className="board">
-      <h1>Tic Tac Toe</h1>
-      <section className="game">
-        {
-          board.map((_, index) => {
-            return (
-              <Square
-                key={index}
-                index={index}
-              >
-                {board[index]}
-              </Square>
-            )
-          })
-        }
-      </section>
-    </main>
+    <h1>Tic Tac Toe</h1>
   )
 }
 
