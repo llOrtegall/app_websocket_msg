db9faf4f4fe6c8a2ea80174d2651ac12eda38270
llOrTeGall <IvanOrtega_97@hotmail.com>
2024-03-08 08:17:50 -0500

    ejemplos de filtrado

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import { useState } from 'react'
 import DataTable from 'react-data-table-component'
 export function App() {
-
   const columns = [
     {
       name: "Nombre",
-      selector: row => row.Nombre
+      selector: row => row.Nombre,
+      sortable: true
     },
     {
       name: "Apellido",
@@ -31,12 +32,215 @@ export function App() {
       Nombre: "Carlos",
       Apellido: "Lopez",
       Edad: 40
+    },
+    {
+      Nombre: "Sebastian",
+      Apellido: "Castro",
+      Edad: 25
+    },
+    {
+      Nombre: "Ana",
+      Apellido: "Salazar",
+      Edad: 56
+    },
+    {
+      Nombre: "Juan",
+      Apellido: "Perez",
+      Edad: 30
+    },
+    {
+      Nombre: "Maria",
+      Apellido: "Gomez",
+      Edad: 25
+    },
+    {
+      Nombre: "Carlos",
+      Apellido: "Lopez",
+      Edad: 40
+    },
+    {
+      Nombre: "Sebastian",
+      Apellido: "Castro",
+      Edad: 25
+    },
+    {
+      Nombre: "Ana",
+      Apellido: "Salazar",
+      Edad: 56
+    },
+    {
+      Nombre: "Juan",
+      Apellido: "Perez",
+      Edad: 30
+    },
+    {
+      Nombre: "Maria",
+      Apellido: "Gomez",
+      Edad: 25
+    },
+    {
+      Nombre: "Carlos",
+      Apellido: "Lopez",
+      Edad: 40
+    },
+    {
+      Nombre: "Sebastian",
+      Apellido: "Castro",
+      Edad: 25
+    },
+    {
+      Nombre: "Ana",
+      Apellido: "Salazar",
+      Edad: 56
+    },    {
+      Nombre: "Juan",
+      Apellido: "Perez",
+      Edad: 30
+    },
+    {
+      Nombre: "Maria",
+      Apellido: "Gomez",
+      Edad: 25
+    },
+    {
+      Nombre: "Carlos",
+      Apellido: "Lopez",
+      Edad: 40
+    },
+    {
+      Nombre: "Sebastian",
+      Apellido: "Castro",
+      Edad: 25
+    },
+    {
+      Nombre: "Ana",
+      Apellido: "Salazar",
+      Edad: 56
+    },
+    {
+      Nombre: "Juan",
+      Apellido: "Perez",
+      Edad: 30
+    },
+    {
+      Nombre: "Maria",
+      Apellido: "Gomez",
+      Edad: 25
+    },
+    {
+      Nombre: "Carlos",
+      Apellido: "Lopez",
+      Edad: 40
+    },
+    {
+      Nombre: "Sebastian",
+      Apellido: "Castro",
+      Edad: 25
+    },
+    {
+      Nombre: "Ana",
+      Apellido: "Salazar",
+      Edad: 56
+    },
+    {
+      Nombre: "Juan",
+      Apellido: "Perez",
+      Edad: 30
+    },
+    {
+      Nombre: "Maria",
+      Apellido: "Gomez",
+      Edad: 25
+    },
+    {
+      Nombre: "Carlos",
+      Apellido: "Lopez",
+      Edad: 40
+    },
+    {
+      Nombre: "Sebastian",
+      Apellido: "Castro",
+      Edad: 25
+    },
+    {
+      Nombre: "Ana",
+      Apellido: "Salazar",
+      Edad: 56
+    },
+    {
+      Nombre: "Juan",
+      Apellido: "Perez",
+      Edad: 30
+    },
+    {
+      Nombre: "Maria",
+      Apellido: "Gomez",
+      Edad: 25
+    },
+    {
+      Nombre: "Carlos",
+      Apellido: "Lopez",
+      Edad: 40
+    },
+    {
+      Nombre: "Sebastian",
+      Apellido: "Castro",
+      Edad: 25
+    },
+    {
+      Nombre: "Ana",
+      Apellido: "Salazar",
+      Edad: 56
+    },    {
+      Nombre: "Juan",
+      Apellido: "Perez",
+      Edad: 30
+    },
+    {
+      Nombre: "Maria",
+      Apellido: "Gomez",
+      Edad: 25
+    },
+    {
+      Nombre: "Carlos",
+      Apellido: "Lopez",
+      Edad: 40
+    },
+    {
+      Nombre: "Sebastian",
+      Apellido: "Castro",
+      Edad: 25
+    },
+    {
+      Nombre: "Ana",
+      Apellido: "Salazar",
+      Edad: 56
     }
   ]
 
+  const [records, setRecords] = useState(data)
+
+  const handleChanged = (e) => {
+      const filteredRecors = data.filter(record => {
+        return record.Nombre.toLowerCase().includes(e.target.value.toLowerCase())
+      })
+
+      setRecords(filteredRecors)
+  } 
+
   return (
     <div>
-      <DataTable columns={columns} data={data} />
+
+      <input type="text" onChange={handleChanged}/>
+
+      <DataTable 
+        columns={columns} 
+        data={records} 
+        selectableRows 
+        // paginationPerPage={4}
+        pagination
+        fixedHeader
+      />  
     </div>
   )
 }
