dce26a5be5f6d79d4f9b13824526ed49d0becd8e
llOrtegall <IvanOrtega_97@hotmail.com>
2024-09-29 14:35:12 -0500

    decode token for ws sokect

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,4 +1,6 @@
-import { PORT, CORS_ORIGIN } from './config';
+import { PORT, CORS_ORIGIN, JWT_SECRET } from './config';
+import { WebSocketServer } from 'ws';
+import { verify } from 'jsonwebtoken';
 import express from 'express';
 import cors from 'cors';
 
@@ -18,6 +20,27 @@ app.get('/', (req, res) => {
 
 app.use('/api/v1', userRoutes)
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
+
+const wss = new WebSocketServer({ server });
+
+wss.on('connection', (socket, req) => {
+  
+  const cookies = req.headers.cookie;
+
+  if (cookies) {
+    const tokenCookieString = cookies.split(';').find((cookie: string) => cookie.includes('token'));
+    if (tokenCookieString) {
+      const token = tokenCookieString.split('=')[1];
+      if (token) {
+        verify(token, JWT_SECRET, {}, async (err: any, decoded: any) => {
+          if (err) throw err;
+          console.log(decoded);
+        });
+      }
+    }
+  }
+
+})
--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -1,4 +1,16 @@
+import { WS_URL } from '../../utils/constanst'
+import { useEffect } from 'react'
+
 export default function Home() {
+
+  useEffect(() => {
+    const ws = new WebSocket(`${WS_URL}`)
+
+    ws.onopen = () => {
+      console.log('Connected to WS server')
+    }
+  }, [])
+
   return(
     <>
       <h1>Hello, world!</h1>
