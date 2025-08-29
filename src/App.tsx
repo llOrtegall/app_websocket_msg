af3500d783de32e0d4aaa4b3628955554bbddf17
llortegall <ivanortega_97@hotmail.com>
2025-08-29 11:40:18 -0500

    refactor: format documents using biome format --write

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,7 @@ function App() {
   return (
     <section className="h-screen w-full bg-linear-to-t from-indigo-950 to-indigo-700">
       <h1>Hello, World!</h1>
-      <Button>
-        Click Me
-      </Button>
+      <Button>Click Me</Button>
     </section>
   );
 }
