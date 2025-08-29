dd04832c23fe9cc383bd82ebdf42f4c81ab201c6
llortegall <ivanortega_97@hotmail.com>
2025-08-29 11:35:39 -0500

    init proyect whit react and vite

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <section className="h-screen w-full bg-linear-to-t from-indigo-950 to-indigo-700">
+      <h1>Hello, World!</h1>
+      <Button>
+        Click Me
+      </Button>
+    </section>
+  );
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import { createRoot } from "react-dom/client";
+import { StrictMode } from "react";
+import App from "./App.tsx";
+import "./index.css";
+
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error("Failed to find root element");
+}
+
+createRoot(rootElement).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
